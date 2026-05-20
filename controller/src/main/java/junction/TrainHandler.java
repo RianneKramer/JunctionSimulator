4afd7c2987a42874033394172c41f1c4480b155f
Rianne Kramer <rianjusss@gmail.com>
2026-05-20 13:55:45 +0200

    Fix timing

--- controller/src/main/java/junction/TrainHandler.java
+++ controller/src/main/java/junction/TrainHandler.java
@@ -54,8 +54,8 @@ public class TrainHandler {
 
         if (currentTimestamp < passTimestamp - SB_ORANGE_BEFORE_ARRIVAL_MS) return 2;
         if (currentTimestamp < passTimestamp - SB_RED_BEFORE_ARRIVAL_MS) return 1;
-        // if (currentTimestamp < passTimestamp + SB_ORANGE_AFTER_PASS_MS) return 0;
-        if (currentTimestamp < passTimestamp + TRAFFIC_RELEASE_AFTER_PASS_MS + SB_ORANGE_AFTER_PASS_MS) return 1;
+        if (currentTimestamp < passTimestamp + SB_ORANGE_AFTER_PASS_MS) return 0;
+        if (currentTimestamp < passTimestamp + TRAFFIC_RELEASE_AFTER_PASS_MS ) return 0;
         return 2;
     }
 
